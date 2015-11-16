daed6264fde3d872e4d6b2b951c85344e33db28d
Bjorn Stange <bjorn248@gmail.com>
2015-11-16 10:15:57 -0500

    added error handling if environment variables are not set

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"github.com/fabioxgn/go-bot"
+	"log"
 	"os"
 	"strings"
 )
 
 func main() {
+
+	if os.Getenv("TWITCH_OAUTH_TOKEN") == "" {
+		log.Fatal("TWITCH_OAUTH_TOKEN not set")
+	}
+	if os.Getenv("TWITCH_CHANNEL_NAME") == "" {
+		log.Fatal("TWITCH_CHANNEL_NAME not set")
+	}
+
 	bot.Run(&bot.Config{
 		Server:   "irc.twitch.tv:6667",
 		Channels: strings.Split(os.Getenv("TWITCH_CHANNEL_NAME"), ","),
