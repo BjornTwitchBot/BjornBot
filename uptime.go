82f727721b12e6eb9631c640782964dfead7c1ad
Bjorn Stange <bjorn248@gmail.com>
2015-11-23 13:20:14 -0500

    added uptime command

---
+++ uptime.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/BjornTwitchBot/BjornBot/Godeps/_workspace/src/github.com/fabioxgn/go-bot"
+	"github.com/BjornTwitchBot/BjornBot/Godeps/_workspace/src/github.com/mrshankly/go-twitch/twitch"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func uptime(command *bot.Cmd) (msg string, err error) {
+
+	client := twitch.NewClient(&http.Client{})
+
+	stream, err := client.Streams.Channel("bjorn_248")
+
+	if err != nil {
+		msg = fmt.Sprintf("Error retrieving stream information from Twitch API")
+		return
+	}
+
+	if stream.Stream.Id == 0 {
+		msg = fmt.Sprintf("Stream is currently offline")
+		return
+	}
+
+	streamStartTime, _ := time.Parse(time.RFC3339, stream.Stream.CreatedAt)
+
+	streamUptime := time.Since(streamStartTime)
+
+	// Remove millseconds from Duration, we don't need that level of precision
+	// This doesn't round, it just flat out removes everything after the dot
+	uptimeString := fmt.Sprintf(strings.Split(streamUptime.String(), ".")[0])
+
+	msg = fmt.Sprintf("Stream Uptime: %vs\n", uptimeString)
+
+	return
+}
+
+func init() {
+	bot.RegisterCommand(
+		"uptime",
+		"Returns the Channel Uptime",
+		"",
+		uptime)
+}
