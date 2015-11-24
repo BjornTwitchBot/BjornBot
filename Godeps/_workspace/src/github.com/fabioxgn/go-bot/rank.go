e5b4cc5a508db9635f4a9dc79a14353fae3f8f57
Bjorn Stange <bjorn248@gmail.com>
2015-11-24 16:36:39 -0500

    removing uneccesary sprintfs

--- Godeps/_workspace/src/github.com/fabioxgn/go-bot/rank.go
+++ Godeps/_workspace/src/github.com/fabioxgn/go-bot/rank.go
@@ -1,14 +1,10 @@
 package bot
 
-import (
-	"fmt"
-)
-
 const (
 	rankCommand = "rank"
 )
 
 func rank(c *Cmd, channel, senderNick string, conn connection) {
-	conn.Privmsg(channel, fmt.Sprintf("League of Legends - Gold V"))
-	conn.Privmsg(channel, fmt.Sprintf("CS:GO - Legendary Eagle Master"))
+	conn.Privmsg(channel, "League of Legends - Gold V")
+	conn.Privmsg(channel, "CS:GO - Legendary Eagle Master")
 }
