fba546db33d833bdb8978c66d640ad4991697879
Bjorn Stange <bjorn248@gmail.com>
2015-12-06 05:37:53 -0500

    Updating CSGO Rank to SMFC

--- Godeps/_workspace/src/github.com/fabioxgn/go-bot/rank.go
+++ Godeps/_workspace/src/github.com/fabioxgn/go-bot/rank.go
@@ -6,5 +6,5 @@ const (
 
 func rank(c *Cmd, channel, senderNick string, conn connection) {
 	conn.Privmsg(channel, "League of Legends - Gold V")
-	conn.Privmsg(channel, "CS:GO - Legendary Eagle Master")
+	conn.Privmsg(channel, "CS:GO - Supreme Master First Class")
 }
