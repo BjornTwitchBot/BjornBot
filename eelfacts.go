e36d5f800b525cc6681da008e0a180d1a3d9f7c8
Bjorn Stange <bjorn248@gmail.com>
2019-11-08 16:14:31 -0500

    add a moray

--- eelfacts.go
+++ eelfacts.go
@@ -7,7 +7,7 @@ import (
 )
 
 func eelfacts(command *bot.Cmd) (msg string, err error) {
-	eelFacts := [19]string{
+	eelFacts := [20]string{
 		"There are more than 700 different kinds, or species, of eels",
 		"Depending on the species, eels can grow to be anywhere between 4 inches to 11 1/2 feet long",
 		"Eels are smooth",
@@ -27,6 +27,7 @@ func eelfacts(command *bot.Cmd) (msg string, err error) {
 		"Garden eel's live in burrows on the sea floor and get their name from their practice of poking their heads from their burrows while most of their bodies remain hidden. Since they tend to live in groups, the many eel heads 'growing' from the sea floor resemble the plants in a garden. The largest can be as much as an acre!",
 		"Reef-associated roving coral groupers have been observed to recruit giant morays to help them hunt. The invitation to hunt is initiated by head-shaking. This style of hunting may allow morays to flush prey from niches not accessible to groupers",
 		"Ribbon eels are carnivores, preying on small fish and other marine creatures.  They can attract their prey with their flared nostrils and then clamp down on them with their strong jaws and retreat into their burrows.",
+		"When you swim in a creek, and an eel bites your cheek, that’s a moray",
 	}
 
 	rand.Seed(time.Now().UnixNano())
